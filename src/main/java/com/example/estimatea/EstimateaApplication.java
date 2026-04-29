2ae3e7221dc476670123776e6c215a897bc9bb8e
Simon BC <simon.bech.christensen.00@gmail.com>
2026-04-29 12:56:29 +0200

    created repo

---
+++ src/main/java/com/example/estimatea/EstimateaApplication.java
@@ -0,0 +1,13 @@
+package com.example.estimatea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstimateaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EstimateaApplication.class, args);
+    }
+
+}
