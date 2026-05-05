cb45ec372bcd78c760af039f8af6261221f04694
Mowgli-dk <emiland4@gmail.com>
2026-05-05 13:48:12 +0200

    little fix in data.sql

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -1,6 +1,6 @@
 INSERT INTO employee VALUES ("Joakim", "Joes", "123", 1);
 INSERT INTO employee VALUES ("Simon", Sich, "007", 2);
-INSERT INTO employee VALUES ("Emil", "Eman", "010", 3)
+INSERT INTO employee VALUES ("Emil", "Eman", "010", 3);
 
 -- Leadership and Management
 INSERT INTO role VALUES
