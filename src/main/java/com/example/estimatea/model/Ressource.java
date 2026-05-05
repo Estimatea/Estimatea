d9075359a3374f3263bf914d4a398805a1540d26
Mowgli-dk <emiland4@gmail.com>
2026-05-05 10:43:48 +0200

    Task & Ressource added to model

---
+++ src/main/java/com/example/estimatea/model/Ressource.java
@@ -0,0 +1,37 @@
+package com.example.estimatea.model;
+
+public class Ressource {
+    int ressourceId;
+    String ressourceName;
+    int ressourceRate;
+
+    public Ressource(int ressourceId, String ressourceName, int ressourceRate) {
+        this.ressourceId = ressourceId;
+        this.ressourceName = ressourceName;
+        this.ressourceRate = ressourceRate;
+    }
+
+    public int getRessourceId() {
+        return ressourceId;
+    }
+
+    public void setRessourceId(int ressourceId) {
+        this.ressourceId = ressourceId;
+    }
+
+    public String getRessourceName() {
+        return ressourceName;
+    }
+
+    public void setRessourceName(String ressourceName) {
+        this.ressourceName = ressourceName;
+    }
+
+    public int getRessourceRate() {
+        return ressourceRate;
+    }
+
+    public void setRessourceRate(int ressourceRate) {
+        this.ressourceRate = ressourceRate;
+    }
+}
