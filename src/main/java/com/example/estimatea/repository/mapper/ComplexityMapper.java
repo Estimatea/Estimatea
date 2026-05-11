9eaae2e7d0f576297cf34cfc92233a69da038e5e
Joakim700 <Joakim.b.eskildsen@gmail.com>
2026-05-11 11:00:27 +0200

    complexityMapper done

---
+++ src/main/java/com/example/estimatea/repository/mapper/ComplexityMapper.java
@@ -0,0 +1,21 @@
+package com.example.estimatea.repository.mapper;
+
+import com.example.estimatea.model.Complexity;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Component
+public class ComplexityMapper implements RowMapper<Complexity> {
+
+    @Override
+    public Complexity mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Complexity complexityScore = new Complexity();
+        complexityScore.setEstimateId(rs.getInt("estimates_id"));
+        complexityScore.setTaskComplexity(rs.getInt("complexity_score"));
+        complexityScore.setTaskId(rs.getInt("task_id"));
+        return complexityScore;
+    }
+}
