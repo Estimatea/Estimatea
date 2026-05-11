4254f32673d118f507b1c43afb03821583f1ae93
Joakim700 <Joakim.b.eskildsen@gmail.com>
2026-05-11 09:35:08 +0200

    SQL add and remove statements for employee repo

--- src/main/java/com/example/estimatea/repository/jdbc/EmployeeRepository.java
+++ src/main/java/com/example/estimatea/repository/jdbc/EmployeeRepository.java
@@ -8,8 +8,20 @@ public class EmployeeRepository {
     private final JdbcTemplate jdbc;
     private final EmployeeMapper employeeMapper;
 
+    // SQL STATEMENTS FOR project_employee Linked to a Project
+    private final String ADD_EMPLOYEE_TO_PROJECT = "INSERT INTO project_employee_junction (employee_id, project_id) VALUES (?, ?)";
+    private final String REMOVE_EMPLOYEE_FROM_PROJECT = "DELETE FROM project_employee WHERE project_employee_id = ?";
+
+    // SQL STATEMENTS FOR sub_project_employee Linked to a Sub-Project
+    private final String ADD_EMPLOYEE_TO_SUB_PROJECT = "INSERT INTO sub_project_employee_junction (project_employee_id, sub_id) VALUES (?, ?)";
+    private final String REMOVE_EMPLOYEE_FROM_SUB_PROJECT = "DELETE FROM sub_project_employee_junction WHERE project_employee_id = ? AND sub_id = ?";
+
+
     public EmployeeRepository(JdbcTemplate jdbc) {
         this.jdbc = jdbc;
         this.employeeMapper = employeeMapper;
     }
+
+
+
 }
