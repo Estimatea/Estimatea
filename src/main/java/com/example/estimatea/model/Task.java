2c29a1372dff8a3c8e259de703459dc15f2c2d52
Mowgli-dk <emiland4@gmail.com>
2026-05-12 09:52:01 +0200

    startDate & boolean completed added to Project, SubProject & Task

--- src/main/java/com/example/estimatea/model/Task.java
+++ src/main/java/com/example/estimatea/model/Task.java
@@ -1,5 +1,7 @@
 package com.example.estimatea.model;
 
+import java.time.LocalDate;
+
 public class Task {
     private int taskId;
     private String taskName;
@@ -7,18 +9,22 @@ public class Task {
     private int taskPrice;
     private int projectId;
     private int ressourceId;
+    private LocalDate startDate;
+    private LocalDate deadLine;
+    private boolean completed;
 
-    public Task () {
-
-    }
-
-    public Task(int taskId, String taskName, int taskTime, int taskPrice, int projectId, int ressourceId) {
+    public Task(int taskId, String taskName, int taskTime, int taskPrice, int projectId, int ressourceId,  LocalDate startDate, LocalDate deadLine, boolean completed) {
         this.taskId = taskId;
         this.taskName = taskName;
         this.taskTime = taskTime;
         this.taskPrice = taskPrice;
         this.projectId = projectId;
         this.ressourceId = ressourceId;
+        this.startDate = startDate;
+        this.deadLine = deadLine;
+    }
+
+    public Task () {
     }
 
     public int getTaskId() {
@@ -67,5 +73,29 @@ public class Task {
 
     public void setRessourceId(int ressourceId) {
         this.ressourceId = ressourceId;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getDeadLine() {
+        return deadLine;
+    }
+
+    public void setDeadLine(LocalDate deadLine) {
+        this.deadLine = deadLine;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
     }
 }
