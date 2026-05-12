d1daa363314e399e8290958dd14fb0412b5568a6
Mowgli-dk <emiland4@gmail.com>
2026-05-12 13:04:17 +0200

    quick fix in TaskRepo

--- src/main/java/com/example/estimatea/repository/mapper/TaskMapper.java
+++ src/main/java/com/example/estimatea/repository/mapper/TaskMapper.java
@@ -19,9 +19,8 @@ public class TaskMapper implements RowMapper<Task> {
         task.setTaskPrice(rs.getInt("task_price"));
         task.setProjectId(rs.getInt("project_id"));
         task.setSubprojectId(rs.getInt("subproject_id"));
-        task.setRessourceId(rs.getInt("ressource_id"));
         task.setStartDate(rs.getDate("start_date").toLocalDate());
-        task.setDeadLine(rs.getDate("dead_line").toLocalDate());
+        task.setDeadline(rs.getDate("deadline").toLocalDate());
         task.setCompleted(rs.getBoolean("completed"));
         return task;
     }
