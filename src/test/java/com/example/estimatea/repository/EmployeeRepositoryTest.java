bbe3ebb4cd4182ca71d9f294f800d0aabdbacc3a
Mowgli-dk <emiland4@gmail.com>
2026-05-12 14:52:41 +0200

    EmployeeRepoTest class created

---
+++ src/test/java/com/example/estimatea/repository/EmployeeRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.example.estimatea.repository;
+
+
+import com.example.estimatea.repository.jdbc.EmployeeRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.net.URL;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class EmployeeRepositoryTest {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    private JdbcTemplate jdbc;
+
+    @Test
+    void contextLoads() {}
+
+    @Test
+    void checkH2schemaFile() {
+        URL url = getClass().getClassLoader().getResource("h2init.sql");
+        System.out.println("URL= " + url);
+    }
+
+}
