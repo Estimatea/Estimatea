4aba983d19de970e184b3969ac56ee9a2afa02a8
Mowgli-dk <emiland4@gmail.com>
2026-05-13 13:31:12 +0200

    EmployeeRepoTest started

--- src/test/java/com/example/estimatea/repository/EmployeeRepositoryTest.java
+++ src/test/java/com/example/estimatea/repository/EmployeeRepositoryTest.java
@@ -1,6 +1,8 @@
 package com.example.estimatea.repository;
 
 
+import com.example.estimatea.model.Employee;
+import com.example.estimatea.model.Project;
 import com.example.estimatea.repository.jdbc.EmployeeRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +15,7 @@ import java.net.URL;
 @SpringBootTest
 @ActiveProfiles("test")
 public class EmployeeRepositoryTest {
+
     @Autowired
     private EmployeeRepository employeeRepository;
 
@@ -26,6 +29,29 @@ public class EmployeeRepositoryTest {
     void checkH2schemaFile() {
         URL url = getClass().getClassLoader().getResource("h2init.sql");
         System.out.println("URL= " + url);
+    }
+
+    @Test
+    void addEmployeeToProjectTest() {
+
+        jdbc.update("INSERT INTO role (role_type, role_rate) VALUES (?, ?)", "Developer", 100);
+
+        jdbc.update("INSERT INTO employee (employee_name, employee_username, employee_password, role_id) VALUES (?, ?, ?, ?)",
+                "John Doe", "johndoe", "password123", 1);
+
+        jdbc.update("INSERT INTO project (project_name, start_date, completed, sum_time, sum_price, deadline, project_manager) VALUES (?, ?, ?, ?, ?, ?, ?)",
+                "Test Project", "2024-01-01", false, 0, 0, "2024-12-31", 1);
+
+        Employee employee = new Employee();
+        employee.setEmployeeId(1);
+
+        Project project = new Project();
+        project.setProjectId(1);
+
+        employeeRepository.addEmployeeToProject(employee, project);
+
+
+
     }
 
 }
