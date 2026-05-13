1c2ce0490e0d75fde3694378d26481ae4e24aaa5
Joakim700 <Joakim.b.eskildsen@gmail.com>
2026-05-13 11:37:20 +0200

    Complexity model and repo work

--- src/main/java/com/example/estimatea/repository/jdbc/ComplexityRepository.java
+++ src/main/java/com/example/estimatea/repository/jdbc/ComplexityRepository.java
@@ -1,46 +1,45 @@
-//package com.example.estimatea.repository.jdbc;
-//
-//import com.example.estimatea.model.Complexity;
-//import com.example.estimatea.repository.mapper.ComplexityMapper;
-//import org.springframework.jdbc.core.JdbcTemplate;
-//import org.springframework.stereotype.Repository;
-//
-//import java.util.List;
-//
-//@Repository
-//public class ComplexityRepository {
-//
-//    private final JdbcTemplate jdbc;
-//    private final ComplexityMapper complexityMapper;
-//
-//    // SQL statements for complexity table Links to task
-//    private final String SHOW_COMPLEXITY_SCORE_BY_ID = "SELECT * FROM complexity WHERE task_id = ?";
-//    private final String CREATE_COMPLEXITY_SCORE = "INSERT INTO complexity (complexity_score) VALUES (?)";
-//    private final String DELETE_COMPLEXITY_SCORE_FROM_TASK = "DELETE FROM task_complexity WHERE complexity_id = ? AND task_id = ?"; // Junction table
-//    private final String UPDATE_COMPLEXITY_SCORE_ON_TASK = "UPDATE complexity SET complexity_score = ? WHERE task_id = ?";
-//
-//    // SQL sorting statements for complexity in (GANTT Diagram)
-//
-//
-//    public ComplexityRepository(JdbcTemplate jdbc, ComplexityMapper complexityMapper) {
-//        this.jdbc = jdbc;
-//        this.complexityMapper = complexityMapper;
-//    }
-//
-//    // CRUD Query's for complexity table
-//    public List<Complexity> showComplexityScoreById(int complexityId) {
-//        return jdbc.query(SHOW_COMPLEXITY_SCORE_BY_ID, complexityMapper, complexityId);
-//    }
-//
-//    public void createComplexityScore(Complexity complexity) {
-//        jdbc.update(CREATE_COMPLEXITY_SCORE, complexityMapper, complexity.getTaskComplexity());
-//    }
-//
-//    public void deleteComplexityScore(int complexityId, int taskId) {
-//        jdbc.update(DELETE_COMPLEXITY_SCORE_FROM_TASK, complexityId, taskId);
-//    }
-//
-//    public void updateComplexityScoreOnTask(Complexity complexity, int taskId) {
-//        jdbc.update(UPDATE_COMPLEXITY_SCORE_ON_TASK, complexityMapper, complexity.getTaskComplexity(), taskId);
-//    }
-//}
+package com.example.estimatea.repository.jdbc;
+
+import com.example.estimatea.model.Complexity;
+import com.example.estimatea.repository.mapper.ComplexityMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class ComplexityRepository {
+
+    private final JdbcTemplate jdbc;
+    private final ComplexityMapper complexityMapper;
+
+    // SQL statements for complexity table Links to task
+    private final String SHOW_COMPLEXITY_SCORE_BY_ID = "SELECT complexity_id, complexity_score, label_type, rate_multiplier FROM complexity WHERE complexity_id = ?";
+    private final String CREATE_COMPLEXITY_SCORE = "INSERT INTO complexity (complexity_score, label_type, rate_multiplier) VALUES (?, ?, ?)";
+    private final String DELETE_COMPLEXITY_SCORE_FROM_TASK = "DELETE FROM task WHERE task_id = ? AND current_complexity_id = ?";
+    private final String UPDATE_CURRENT_COMPLEXITY_SCORE_ON_TASK = "UPDATE task SET current_complexity_id = ? WHERE task_id = ?";
+
+    // SQL sorting statements for complexity in (GANTT Diagram)
+
+    public ComplexityRepository(JdbcTemplate jdbc, ComplexityMapper complexityMapper) {
+        this.jdbc = jdbc;
+        this.complexityMapper = complexityMapper;
+    }
+
+    // CRUD Query's for complexity table
+    public Complexity showComplexityScoreById(int currentComplexityId) {
+        return jdbc.queryForObject(SHOW_COMPLEXITY_SCORE_BY_ID, complexityMapper, currentComplexityId);
+    }
+
+    public void createComplexityScore(Complexity complexity) {
+        jdbc.update(CREATE_COMPLEXITY_SCORE, complexity.getComplexityScore(), complexity.getLabelType(), complexity.getRateMultiplier());
+    }
+
+    public void deleteComplexityScore(int taskId, int currentComplexityId) {
+        jdbc.update(DELETE_COMPLEXITY_SCORE_FROM_TASK, taskId, currentComplexityId);
+    }
+
+    public void updateComplexityScoreOnTask(int taskId) {
+        jdbc.update(UPDATE_CURRENT_COMPLEXITY_SCORE_ON_TASK, taskId);
+    }
+}
