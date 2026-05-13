6d2b1c5338515790a4bd41cbb0ff17df7166aae4
Mowgli-dk <emiland4@gmail.com>
2026-05-13 13:47:14 +0200

    TaskRepoTest started

--- src/test/java/com/example/estimatea/repository/TaskRepositoryTest.java
+++ src/test/java/com/example/estimatea/repository/TaskRepositoryTest.java
@@ -1,4 +1,36 @@
 package com.example.estimatea.repository;
 
+
+import com.example.estimatea.repository.jdbc.EmployeeRepository;
+import com.example.estimatea.repository.jdbc.ProjectRepository;
+import com.example.estimatea.repository.jdbc.TaskRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.net.URL;
+
+@SpringBootTest
+@ActiveProfiles("test")
 public class TaskRepositoryTest {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @Autowired
+    private JdbcTemplate jdbc;
+
+    @Test
+    void contextLoads() {}
+
+    @Test
+    void checkH2schemaFile(){
+        URL url = getClass().getClassLoader().getResource("h2init.sql");
+        System.out.println("URL= " + url);
+
+
+
+    }
 }
