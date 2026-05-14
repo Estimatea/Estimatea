718d8b4ac590d152c3ff9488ac6e5ff9d82b3785
Joakim700 <Joakim.b.eskildsen@gmail.com>
2026-05-14 18:56:27 +0200

    EmployeeRepo Test Finished

--- src/main/java/com/example/estimatea/repository/jdbc/EmployeeRepository.java
+++ src/main/java/com/example/estimatea/repository/jdbc/EmployeeRepository.java
@@ -14,26 +14,29 @@ public class EmployeeRepository {
     private final JdbcTemplate jdbc;
     private final EmployeeMapper employeeMapper;
 
-        // SQL STATEMENTS FOR project_employee Linked to a Project
+        //SQL STATEMENTS FOR employees
+    private final String GET_COMPANY_EMPLOYEE_LIST = "SELECT * FROM employee";
 
+        // SQL STATEMENTS FOR project_employee Linked to a Project
     private final String GET_ALL_PROJECT_EMPLOYEES_BY_ID_FOR_PROJECT = "SELECT * FROM project_employee WHERE project_id = ?"; // RETRIEVE Project employees by ID's
-    private final String GET_ALL_EMPLOYEES_FOR_PROJECT = "SELECT * FROM project_employee WHERE project_id = ?";
+    private final String GET_ALL_EMPLOYEES_FOR_PROJECT = "SELECT e.* FROM employee e " +
+                                                         "JOIN project_employee pe ON e.employee_id = pe.employee_id " +
+                                                         "WHERE pe.project_id = ?";
 
     // ADD & REMOVE employees to/from project
     private final String ADD_EMPLOYEE_TO_PROJECT = "INSERT INTO project_employee (employee_id, project_id) VALUES (?, ?)";
     private final String REMOVE_EMPLOYEE_FROM_PROJECT = "DELETE FROM project_employee WHERE project_employee_id = ? AND project_id = ?";
 
         // SQL statements for subproject employees
-
     // CHECK IN SERVICE THAT THEY ARE PROJECT EMPLOYEES
     private final String GET_ALL_EMPLOYEES_BY_ID_FOR_SUBPROJECT = "SELECT project_employee_id FROM sub_project_employee WHERE sub_id = ?"; // RETRIEVE Sub-project employees ID's
-    private final String GET_ALL_EMPLOYEES_FOR_SUBPROJECT = "SELECT e.* FROM employee e" +
-            "                                                JOIN project_employee pe ON e.employee_id = pe.employee_id" +
-            "                                                JOIN sub_project_employee spe ON pe.project_employee_id = spe.project_employee_id" +
-            "                                                WHERE spe.sub_id = ?" ;
+    private final String GET_ALL_EMPLOYEES_FOR_SUBPROJECT = "SELECT e.* FROM employee e " +
+                                                            "JOIN project_employee pe ON e.employee_id = pe.employee_id " +
+                                                            "JOIN sub_project_employee spe ON pe.project_employee_id = spe.project_employee_id " +
+                                                            "WHERE spe.sub_id = ?" ;
 
     // ADD & REMOVE employees to/from Sub-project
-    private final String ASSIGN_EMPLOYEE_TO_SUBPROJECT = "INSERT INTO sub_project_employee VALUES (?,?)";
+    private final String ASSIGN_EMPLOYEE_TO_SUBPROJECT = "INSERT INTO sub_project_employee (project_employee_id, sub_id) VALUES (?,?)";
     private final String REMOVE_EMPLOYEE_FROM_SUBPROJECT = "DELETE FROM sub_project_employee WHERE project_employee_id = ? AND sub_id = ?";
 
     public EmployeeRepository(JdbcTemplate jdbc, EmployeeMapper employeeMapper) {
@@ -41,11 +44,16 @@ public class EmployeeRepository {
         this.employeeMapper = employeeMapper;
     }
 
-    // ROW-MAPPERS for retrieving ID's
+        // ROW-MAPPERS for retrieving ID's
     private final static RowMapper<Integer> ProjectEmpIdMapper = (rs, rowNum) -> rs.getInt("project_employee_id"); // MAIN Project
     private final static RowMapper<Integer> SubProjectEmpIdMapper = (rs, rowNum) -> rs.getInt("project_employee_id"); // SUB - Project
 
-    // CRUD QUERY'S For Main PROJECT
+        // CRUD QUERY's For Employees
+    public List<Employee> getAllEmployeesInCompany() {
+        return jdbc.query(GET_COMPANY_EMPLOYEE_LIST, employeeMapper);
+    }
+
+        // CRUD QUERY'S For Main PROJECT-Employees
     public List<Integer> getAllEmployeeIdsForProject(int projectId) { // Returns a list of employee ID's connected to a project
         return jdbc.query(GET_ALL_PROJECT_EMPLOYEES_BY_ID_FOR_PROJECT, ProjectEmpIdMapper, projectId);
     }
@@ -53,15 +61,16 @@ public class EmployeeRepository {
     public List<Employee> getAllEmployeesForProject(int projectId) {
         return jdbc.query(GET_ALL_EMPLOYEES_FOR_PROJECT, employeeMapper, projectId);
     }
-    public void addEmployeeToProject(Employee employee, Project project) { // Adds an existing employee to a project
-        jdbc.update(ADD_EMPLOYEE_TO_PROJECT, employee.getEmployeeId(), project.getProjectId());
+
+    public void addEmployeeToProject(int employeeId, int projectId) { // Adds an existing employee to a project
+        jdbc.update(ADD_EMPLOYEE_TO_PROJECT, employeeId, projectId);
     }
 
-    public void removeEmployeeFromProject(Employee employee, Project project) {  // Removes an employee from project
-        jdbc.update(REMOVE_EMPLOYEE_FROM_PROJECT, employee.getEmployeeId(), project.getProjectId());
+    public void removeEmployeeFromProject(int employeeId, int projectId) {  // Removes an employee from project
+        jdbc.update(REMOVE_EMPLOYEE_FROM_PROJECT, employeeId, projectId);
     }
 
-    // CRUD QUREY's for SUB_PROJECT employees
+        // CRUD QUREY's for SUB-PROJECT-Employees
     public List<Integer> getAllEmployeesByIdForSubProject(int subProjectId) { // Returns a list of employee ID's connected to a Sub-Project
         return jdbc.query(GET_ALL_EMPLOYEES_BY_ID_FOR_SUBPROJECT, SubProjectEmpIdMapper, subProjectId);
     }
@@ -70,11 +79,11 @@ public class EmployeeRepository {
         return jdbc.query(GET_ALL_EMPLOYEES_FOR_SUBPROJECT, employeeMapper, subProjectId);
     }
 
-    public void assignEmployeeToSubProject(Employee employee, int subProjectId) { // Assigns an employee to the subproject // CHECK IN SERVICE THAT THEY ARE A PROJECT_EMPLOYEE
-        jdbc.update(ASSIGN_EMPLOYEE_TO_SUBPROJECT, employee.getEmployeeId(), subProjectId);
+    public void assignEmployeeToSubProject(int employeeId, int subProjectId) { // Assigns an employee to the subproject // CHECK IN SERVICE THAT THEY ARE A PROJECT_EMPLOYEE
+        jdbc.update(ASSIGN_EMPLOYEE_TO_SUBPROJECT, employeeId, subProjectId);
     }
 
-    public void removeEmployeeFromSubProject(Employee employee, int subProjectId) { // Deletes a specific sub_project_employee from the subproject
-        jdbc.update(REMOVE_EMPLOYEE_FROM_SUBPROJECT, employee.getEmployeeId(), subProjectId);
+    public void removeEmployeeFromSubProject(int employeeId, int subProjectId) { // Deletes a specific sub_project_employee from the subproject
+        jdbc.update(REMOVE_EMPLOYEE_FROM_SUBPROJECT, employeeId, subProjectId);
     }
 }
--- src/test/resources/data-test.sql
+++ src/test/resources/data-test.sql
@@ -1,26 +1,38 @@
 
--- 1. Roles
+-- 1. roles
 INSERT INTO role (role_type, role_rate)
 VALUES ('Project Manager', 2500),
        ('Subproject Manager', 1500),
        ('Program Manager', 1200);
 
--- 2. Employees
+-- 2. employees
 INSERT INTO employee (employee_name, employee_username, employee_password, role_id)
 VALUES ('Joakim', 'joakim_dev', 'password_123', 1),
        ('Emil', 'emil_dev', 'password_456', 2),
-       ('Simon', 'simon_dev', 'password_678', 3);
+       ('Simon', 'simon_dev', 'password_678', 3),
+       ('Jackie', 'jackie_dev', 'password_777', 3);
 
--- 3. Projects
+-- 3. projects
 INSERT INTO project (project_name, start_date, deadline, project_manager)
 VALUES ('Alpha Solutions', '2026-01-01', '2026-12-31', 1);
 
--- 4. Subprojects
+-- 4. subprojects
 INSERT INTO subproject (sub_name, start_date, deadline, completed, project_id)
 VALUES ('Project calculation tool', '2026-02-01', '2026-03-01', false, 1);
 
--- 5. Complexity Scores
+-- 5. complexity_scores
 INSERT INTO complexity (complexity_score, label_type, rate_multiplier)
 VALUES (1, 'Standard', 1.0),
        (5, 'Elevated', 1.5),
        (10, 'Enterprise Grade', 3.0);
+
+-- 6. project_employee
+INSERT INTO project_employee (employee_id, project_id)
+VALUES (1,1), -- Joakim
+       (2,1), -- Emil
+       (3,1); -- Simon
+
+-- 7. subproject_employee
+INSERT INTO sub_project_employee (project_employee_id, sub_id)
+VALUES (1,1), -- Joakim
+       (2,1); -- Emil
