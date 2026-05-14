08b1eaffe5a374ce1404134c4f229e585fcd94fb
Joakim700 <Joakim.b.eskildsen@gmail.com>
2026-05-14 13:49:27 +0200

    employee repo

--- src/main/java/com/example/estimatea/repository/jdbc/EmployeeRepository.java
+++ src/main/java/com/example/estimatea/repository/jdbc/EmployeeRepository.java
@@ -16,8 +16,7 @@ public class EmployeeRepository {
 
         // SQL STATEMENTS FOR project_employee Linked to a Project
 
-    // RETRIEVE BY ID's
-    private final String GET_ALL_PROJECT_EMPLOYEES_BY_ID_FOR_PROJECT = "SELECT * FROM project_employee WHERE project_id = ?";
+    private final String GET_ALL_PROJECT_EMPLOYEES_BY_ID_FOR_PROJECT = "SELECT * FROM project_employee WHERE project_id = ?"; // RETRIEVE Project employees by ID's
     private final String GET_ALL_EMPLOYEES_FOR_PROJECT = "SELECT * FROM project_employee WHERE project_id = ?";
 
     // ADD & REMOVE employees to/from project
@@ -27,8 +26,11 @@ public class EmployeeRepository {
         // SQL statements for subproject employees
 
     // CHECK IN SERVICE THAT THEY ARE PROJECT EMPLOYEES
-    private final String GET_ALL_EMPLOYEES_BY_ID_FOR_SUBPROJECT = "SELECT * FROM sub_project_employee WHERE sub_id = ?"; // RETRIEVE Sub-project employee ID's
-    //private final String GET_ALL_EMPLOYEE_INFO_FOR_SUBPROJECT =;
+    private final String GET_ALL_EMPLOYEES_BY_ID_FOR_SUBPROJECT = "SELECT project_employee_id FROM sub_project_employee WHERE sub_id = ?"; // RETRIEVE Sub-project employees ID's
+    private final String GET_ALL_EMPLOYEES_FOR_SUBPROJECT = "SELECT e.* FROM employee e" +
+            "                                                JOIN project_employee pe ON e.employee_id = pe.employee_id" +
+            "                                                JOIN sub_project_employee spe ON pe.project_employee_id = spe.project_employee_id" +
+            "                                                WHERE spe.sub_id = ?" ;
 
     // ADD & REMOVE employees to/from Sub-project
     private final String ASSIGN_EMPLOYEE_TO_SUBPROJECT = "INSERT INTO sub_project_employee VALUES (?,?)";
@@ -40,16 +42,16 @@ public class EmployeeRepository {
     }
 
     // ROW-MAPPERS for retrieving ID's
-    private final static RowMapper<Integer> ProjectEmpIdMapper = (rs, rowNum) -> rs.getInt("project_employee_id");
-    private final static RowMapper<Integer> SubProjectEmpIdMapper = (rs, rowNum) -> rs.getInt("project_employee_id");
+    private final static RowMapper<Integer> ProjectEmpIdMapper = (rs, rowNum) -> rs.getInt("project_employee_id"); // MAIN Project
+    private final static RowMapper<Integer> SubProjectEmpIdMapper = (rs, rowNum) -> rs.getInt("project_employee_id"); // SUB - Project
 
     // CRUD QUERY'S For Main PROJECT
-    public List<Integer> getAllEmployeeIdsForProject(Project project) { // Returns a list of employee ID's connected to a project
-        return jdbc.query(GET_ALL_PROJECT_EMPLOYEES_BY_ID_FOR_PROJECT, ProjectEmpIdMapper, project.getProjectId());
+    public List<Integer> getAllEmployeeIdsForProject(int projectId) { // Returns a list of employee ID's connected to a project
+        return jdbc.query(GET_ALL_PROJECT_EMPLOYEES_BY_ID_FOR_PROJECT, ProjectEmpIdMapper, projectId);
     }
 
-    public List<Employee> getAllEmployeesForProject(Project project) {
-        return jdbc.query(GET_ALL_EMPLOYEES_FOR_PROJECT, employeeMapper, project.getProjectId());
+    public List<Employee> getAllEmployeesForProject(int projectId) {
+        return jdbc.query(GET_ALL_EMPLOYEES_FOR_PROJECT, employeeMapper, projectId);
     }
     public void addEmployeeToProject(Employee employee, Project project) { // Adds an existing employee to a project
         jdbc.update(ADD_EMPLOYEE_TO_PROJECT, employee.getEmployeeId(), project.getProjectId());
@@ -62,6 +64,10 @@ public class EmployeeRepository {
     // CRUD QUREY's for SUB_PROJECT employees
     public List<Integer> getAllEmployeesByIdForSubProject(int subProjectId) { // Returns a list of employee ID's connected to a Sub-Project
         return jdbc.query(GET_ALL_EMPLOYEES_BY_ID_FOR_SUBPROJECT, SubProjectEmpIdMapper, subProjectId);
+    }
+
+    public List<Employee> getAllEmployeesForSubProject(int subProjectId) {
+        return jdbc.query(GET_ALL_EMPLOYEES_FOR_SUBPROJECT, employeeMapper, subProjectId);
     }
 
     public void assignEmployeeToSubProject(Employee employee, int subProjectId) { // Assigns an employee to the subproject // CHECK IN SERVICE THAT THEY ARE A PROJECT_EMPLOYEE
