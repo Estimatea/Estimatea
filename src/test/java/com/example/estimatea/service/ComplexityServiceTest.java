70d9f02c1c4f6c5f19de98419e7c2f01f742280a
Joakim700 <Joakim.b.eskildsen@gmail.com>
2026-05-18 10:43:16 +0200

    ComplexityServiceTest done

--- src/test/java/com/example/estimatea/service/ComplexityServiceTest.java
+++ src/test/java/com/example/estimatea/service/ComplexityServiceTest.java
@@ -2,10 +2,18 @@ package com.example.estimatea.service;
 
 import com.example.estimatea.model.Complexity;
 import com.example.estimatea.repository.jdbc.ComplexityRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class ComplexityServiceTest {
@@ -15,8 +23,23 @@ public class ComplexityServiceTest {
 
    @InjectMocks
    private ComplexityService complexityService;
-   private Complexity complexity;
+   private Complexity complexityMock;
 
+   @BeforeEach
+   public void setUp() {
+        complexityMock = new Complexity(10, "Enterprise Grade", 3.0);
+   }
 
+   @Test
+    void shouldShowAllComplexityScores() {
+       when(complexityRepository.showAllComplexityScores()).thenReturn(List.of(complexityMock));
 
+       List<Complexity> complexityList = complexityService.showAllComplexityScores();
+
+       assertNotNull(complexityList);
+       assertThat(complexityList.size() == 1);
+       assertThat(complexityList.getFirst().getComplexityScore()).isEqualTo(10);
+       assertThat(complexityList.getFirst().getLabelType()).isEqualTo("Enterprise Grade");
+       assertThat(complexityList.getFirst().getRateMultiplier()).isEqualTo(3.0);
+   }
 }
