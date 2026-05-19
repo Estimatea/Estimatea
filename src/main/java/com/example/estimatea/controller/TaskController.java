bf8d8991565824c1e25b94805bce92904a10dc3c
Mowgli-dk <emiland4@gmail.com>
2026-05-19 13:55:28 +0200

    TaskController & task-form sat op

--- src/main/java/com/example/estimatea/controller/TaskController.java
+++ src/main/java/com/example/estimatea/controller/TaskController.java
@@ -25,34 +25,91 @@ public class TaskController {
         this.subProjectService = subProjectService;
     }
 
-    @GetMapping("/add/project")
-    public String addTaskToProjectForm(@RequestParam int projectId, Model model) {
+    //
+    //Create task for Project
+    @GetMapping("/{projectId}/add/project")
+    public String addTaskToProjectForm(@PathVariable int projectId, Model model) {
         Project currentProject = projectService.findProjectById(projectId);
         model.addAttribute("project", currentProject);
         return "task-form";
-
     }
 
-            @PostMapping("/createTaskForProject")
-            public String createTaskForProject(@ModelAttribute Task task) {
-                taskService.creatTaskForProject(task);
-                return "redirect:/project/";
-            }
+    @PostMapping("/createTaskForProject")
+    public String createTaskForProject(@ModelAttribute Task task) {
+        taskService.createTaskForProject(task);
+        return "redirect:/project/" + task.getProjectId();
+    }
 
-
-
-    @GetMapping("/add/subproject")
-    public String addTaskToSubprojectForm(@RequestParam int subprojectId, Model model) {
-        SubProject currentSubproejct = subProjectService.findSubProjectById(subprojectId);
-        model.addAttribute("subproject", currentSubproejct);
+    //
+    //Create task for Subproject
+    @GetMapping("/{subprojectId}/add/subproject")
+    public String addTaskToSubprojectForm(@PathVariable int subprojectId, Model model) {
+        SubProject currentSubproject = subProjectService.findSubProjectById(subprojectId);
+        model.addAttribute("subproject", currentSubproject);
         return "task-form";
     }
 
-            @PostMapping("/createTaskForSubproejct")
-            public String createTaskForSubproject(@ModelAttribute Task task) {
-                taskService.createTaskForSubproject(task);
-                return "redirect:/subproject";
-            }
+    @PostMapping("/createTaskForSubproject")
+    public String createTaskForSubproject(@ModelAttribute Task task) {
+        taskService.createTaskForSubproject(task);
+        return "redirect:/subproject/" + task.getSubprojectId();
+    }
 
+    //
+    //Edit Task in Project
+    @GetMapping("/{projectId}/{taskId}/edit")
+    public String editTaskInProjectForm(@PathVariable int taskId, Model model) {
+        Task currentTask = taskService.getTaskById(taskId);
+        model.addAttribute("task", currentTask);
+        return "task-form";
+    }
 
+    @PostMapping("/edit/project")
+    public String editTaskInProject(@ModelAttribute Task task) {
+        taskService.editTask(task);
+        return "redirect:/project/" + task.getProjectId();
+    }
+
+    //Edit Task in Subproject
+    @GetMapping("/{subprojectId}/{taskId}/edit")
+    public String editTaskInSubprojectForm(@PathVariable int taskId, Model model) {
+        Task currenTask = taskService.getTaskById(taskId);
+        model.addAttribute("task", currenTask);
+        return "task-form";
+    }
+
+    @PostMapping("/edit/subproject")
+    public String editTaskInSubproject(@ModelAttribute Task task) {
+        taskService.editTask(task);
+        return "redirect:/subproject" + task.getSubprojectId();
+    }
+
+    //
+    //Complete Task in Project
+    @PostMapping("/complete/project")
+    public String completeTaskInProject(@ModelAttribute Task task) {
+        taskService.completeTask(task.getTaskId());
+        return "redirect:/project/" + task.getProjectId();
+    }
+
+    //Complete Task in Subproject
+    @PostMapping("/complete/subproject")
+    public String completeTaskInSubproject(@ModelAttribute Task task) {
+        taskService.completeTask(task.getTaskId());
+        return "redirect:/subproject/" + task.getSubprojectId();
+    }
+
+    //
+    //Delete Task in Project
+    @PostMapping("/delete/project")
+    public String deleteTaskInProject(@ModelAttribute Task task) {
+        taskService.deleteTask(task.getTaskId());
+        return "redirect:/project/" + task.getProjectId();
+    }
+
+    @PostMapping("/delete/subproject")
+    public String deleteTask(@ModelAttribute Task task) {
+        taskService.deleteTask(task.getTaskId());
+        return "redirect:/subproject/" + task.getSubprojectId();
+    }
 }
