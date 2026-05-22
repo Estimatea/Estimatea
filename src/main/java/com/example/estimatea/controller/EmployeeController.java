aa1fe2bd443a66fca8a8f1bd98ea59c46d81dea6
Mowgli-dk <emiland4@gmail.com>
2026-05-22 13:49:11 +0200

    remove-employee-from-project works, remove from sub in progress

--- src/main/java/com/example/estimatea/controller/EmployeeController.java
+++ src/main/java/com/example/estimatea/controller/EmployeeController.java
@@ -1,6 +1,5 @@
 package com.example.estimatea.controller;
 
-import com.example.estimatea.exception.NotFoundException;
 import com.example.estimatea.model.Employee;
 import com.example.estimatea.service.EmployeeService;
 import jakarta.servlet.http.HttpSession;
@@ -31,7 +30,7 @@ public class EmployeeController {
     }
 
     @PostMapping("/login")
-    public String login(@RequestParam String employeeUsername, @RequestParam String employeePassword, HttpSession session, Model model) {
+    public String login(@RequestParam String employeeUsername, @RequestParam String employeePassword, HttpSession session) {
             Employee employee = employeeService.employeeLogin(employeeUsername, employeePassword);
             session.setAttribute("currentEmployee", employee);
             return "redirect:/projects/all";
@@ -69,17 +68,26 @@ public class EmployeeController {
             }
 
     // Remove employee from Main project
-    @PostMapping("/project/remove") // POST
-    public String removeEmployeeFromProject(@RequestParam int employeeId, @RequestParam int projectId) {
-        employeeService.removeEmployeeFromProject(employeeId, projectId);
-        return "redirect:/project";
+    @GetMapping("/remove/project")
+    public String removeEmployeeFromProjectFrom(Model model) {
+        List<Employee> employeeList = employeeService.getAllEmployeeInCompany();
+        model.addAttribute("employeeList", employeeList);
+        return "remove-employee-from-project";
     }
+
+            @PostMapping("/remove/project") // POST
+            public String removeEmployeeFromProject(@RequestParam int employeeId, @RequestParam int projectId) {
+                employeeService.removeEmployeeFromProject(employeeId, projectId);
+                return "redirect:/projects/" + projectId;
+            }
+
+
 
         // Employee handling on Subproject
 
     // Add employee to Subproject
     @GetMapping("/add/subproject") // GET
-    public String addEmployeeToSubProjectForm(@RequestParam int employeeId, @RequestParam int projectId, @RequestParam int subProjectId, Model model) {
+    public String addEmployeeToSubProjectForm(@RequestParam int projectId, @RequestParam int subProjectId, Model model) {
         List<Employee> subProjectEmpList = employeeService.getAllEmployeesByProjectId(projectId);
         model.addAttribute("subProjectEmployeeList", subProjectEmpList);
         model.addAttribute("projectId", projectId);
@@ -91,13 +99,24 @@ public class EmployeeController {
             @PostMapping("add/subproject") // POST
             public String employeeAddedToSubProject(@RequestParam int employeeId, @RequestParam int subProjectId, @RequestParam int projectId) {
                 employeeService.addEmployeeToSubProject(employeeId, subProjectId, projectId);
-                return "redirect:/subprojects" + subProjectId;
+                return "redirect:/subprojects/" + subProjectId;
             }
 
+
     // Remove employee from Subproject
-    @PostMapping("/subproject/remove") // POST
-    public String removeEmployeeFromSubProject(@RequestParam int employeeId, @RequestParam int subProjectId) {
-        employeeService.removeEmployeeFromSubProject(employeeId, subProjectId);
-        return "redirect:/subprojects";
+    @GetMapping("/remove/subproject")
+    public String removeEmployeeFromSubprojectFrom(@RequestParam int projectId, @RequestParam int subProjectId, Model model) {
+        List<Employee> subProjectemployeeList = employeeService.getAllEmployeesByProjectId(projectId);
+        model.addAttribute("subProjectEmployeeList", subProjectemployeeList);
+        model.addAttribute("projectId", projectId);
+        model.addAttribute("subProjectId", subProjectId);
+
+        return "remove-employee-from-project";
     }
+
+            @PostMapping("/remove/subproject") // POST
+            public String removeEmployeeFromSubProject(@RequestParam int employeeId,@RequestParam int subProjectId) {
+                employeeService.removeEmployeeFromSubProject(employeeId, subProjectId);
+                return "redirect:/subproject/" + subProjectId;
+            }
 }
