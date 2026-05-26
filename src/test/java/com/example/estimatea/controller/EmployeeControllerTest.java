bab29abddfb2bd93b21c76eb397b86dcae7c22f4
Joakim700 <Joakim.b.eskildsen@gmail.com>
2026-05-26 16:26:59 +0200

    syntax cleanup

--- src/test/java/com/example/estimatea/controller/EmployeeControllerTest.java
+++ src/test/java/com/example/estimatea/controller/EmployeeControllerTest.java
@@ -22,7 +22,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(EmployeeController.class)
-@AutoConfigureMockMvc(addFilters = false)
 public class EmployeeControllerTest {
 
     @Autowired
